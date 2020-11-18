a802b10039af1cf8c6c496b63a4fbc0bf54f3358
akuma-phawx <chrisvradis00@gmail.com>
2020-11-18 22:42:14 +0200

    Reviews Validation

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
-const { campgroundSchema } = require("./schemas");
+const { campgroundSchema, reviewSchema } = require("./schemas");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
+const Review = require("./models/review");
 const { stat } = require("fs");
 
 //connecting to mongo
@@ -38,7 +39,17 @@ app.use(methodOverride("_method"));
 const validateCampground = (req, res, next) => {
   const { error } = campgroundSchema.validate(req.body);
   if (error) {
-    const msg = error.details.map((el) => el.message.join(","));
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
     throw new ExpressError(msg, 400);
   } else {
     next();
@@ -115,6 +126,22 @@ app.get(
     const { id } = req.params;
     const camp = await Campground.findById(id);
     res.render("campgrounds/show", { camp });
+  })
+);
+
+app.post(
+  "/campgrounds/:id/reviews",
+  validateReview,
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const camp = await Campground.findById(id);
+    const review = new Review(req.body.review);
+    // log(camp);
+    camp.reviews.push(review);
+    await review.save();
+    await camp.save();
+    // log(camp);
+    res.redirect(`/campgrounds/${id}`);
   })
 );
 
