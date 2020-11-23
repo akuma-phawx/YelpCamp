d4841f55aa38678eea7f7b034249a4a1b525b1be
akuma-phawx <chrisvradis00@gmail.com>
2020-11-23 20:07:55 +0200

    Cookie Protection // Hiding Development errors

--- app.js
+++ app.js
@@ -46,11 +46,13 @@ app.use(methodOverride("_method"));
 app.use(mongoSanitize());
 
 const sessionConfig = {
+  name: "session",
   secret: "thisshouldbeabettersecret",
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
+    // secure : true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
